393ab5ff2f4c9a9f54db5231980a08fc014297f2
Mark Mullins <markmullins95@gmail.com>
2022-08-13 11:54:21 -0400

    feat: add sitemap.xml for EZ-5

---
+++ apps/public-ui/pages/sitemap.xml.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Sitemap = () => {
+  return null;
+};
+
+export const getServerSideProps = async ({ res }) => {
+  const BASE_URL = 'https://ezsailingcharters.com';
+
+  const paths = [
+    '',
+    'charters',
+    'special-occasions',
+    'story',
+    'faq',
+    'catering',
+  ];
+
+  const urls = paths.map(
+    (path) =>
+      `<url>
+        <loc>${BASE_URL}/${path}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </url>`
+  );
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    ${urls.join('')}
+    </urlset>
+  `;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
