51d028873ff95935f379bb4cef12ebbc3351ca7f
Chad Gallati <chad@gallati.dev>
2022-09-08 17:50:14 -0400

    fix: make it a public env var so it's available client side

--- apps/public-ui/components/Form/PaymentFormlet/WithStripeElement.tsx
+++ apps/public-ui/components/Form/PaymentFormlet/WithStripeElement.tsx
@@ -2,7 +2,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import { ReactElement } from 'react';
 
-const stripePromise = loadStripe(process.env.STRIPE_KEY);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
 export const WithStripeElement: React.FC<{
   secret: string;
