5346b1b0d50972badd05cb51cd54e5576dfa6fbc
Chad Gallati <chad@gallati.dev>
2022-11-26 17:24:18 -0500

    fix: allow avail calendar to be advanced in admin mode

--- apps/admin-ui/pages/availability.tsx
+++ apps/admin-ui/pages/availability.tsx
@@ -8,6 +8,15 @@ import {
 import { AvailabilityForm } from '../components/Forms';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { deserializeAvailability } from '../lib/data-access';
+import {
+  addMonths,
+  addYears,
+  getDaysInMonth,
+  getMonth,
+  getYear,
+  isBefore,
+} from 'date-fns';
+import { getTimezoneOffset } from 'date-fns-tz';
 
 type AvailabilityPageProps = {
   availability: SerializableMonth[];
@@ -34,6 +43,28 @@ const AvailabilityPage: NextPage<AvailabilityPageProps> = ({
 export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
   getServerSideProps: async (_) => {
     const months = await fetchAndFormatAvailability();
+    const endDate = addYears(new Date(), 1);
+    let dateCursor = new Date(months[months.length - 1].firstDate);
+    while (isBefore(dateCursor, endDate)) {
+      const daysInMonth = getDaysInMonth(dateCursor);
+      const days = [];
+      for (let i = 1; i <= daysInMonth; i++) {
+        const availsToday = [];
+        days.push({
+          avails: availsToday,
+        });
+      }
+      months.push({
+        firstDate: addMonths(dateCursor, 1).toString(),
+        firstDateOffsetHours: (getTimezoneOffset(
+          'America/New_York',
+          dateCursor
+        ) /
+          (1_000 * 60 * 60)) as -4 | -5,
+        days,
+      });
+      dateCursor = new Date(months[months.length - 1].firstDate);
+    }
     return {
       props: {
         availability: months,
