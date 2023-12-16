7de198e5ba560a4bbaad202914232eb391e2546a
Chad Gallati <chadgallati@Chads-MBP.Hargray_NVG468MQ_GPON_DataIPTV_v5>
2023-12-16 17:27:08 -0500

    feat: finish up the blog work

--- apps/public-ui/components/FeedPage/PostListItem.tsx
+++ apps/public-ui/components/FeedPage/PostListItem.tsx
@@ -6,13 +6,11 @@ const PostListItemContainer = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  //border: 1px solid #ddd;
   max-width: 800px;
-  cursor: pointer;
-  margin-bottom: 2rem;
-
+  margin-bottom: 5rem;
+  padding: 0 20px;
   @media (min-width: 600px) {
-    font: normal normal 400 17px/32px Source Sans Pro;
+    //font: normal normal 400 17px/32px Source Sans Pro;
     flex-direction: column;
   }
 `;
@@ -20,51 +18,69 @@ const PostListItemContainer = styled.section`
 const PostDetails = styled.div`
   display: flex;
   flex-direction: column;
-  //border: 1px solid #ddd;
   flex-basis: 60%;
   justify-content: center;
 `;
 
+const ImageWrapper = styled.div`
+  position: relative;
+  max-width: 640px;
+  margin: auto; // Centers the wrapper on the page
+  width: 100%;
+  padding-bottom: 50%;
+`;
+
+
 const PostTitle = styled.h2`
-  font: normal normal 600 32px/30px Source Sans Pro;
+  font: normal normal normal 20px/18px Source Sans Pro;
+  cursor: pointer;
   letter-spacing: 2px;
   color: #001d2c;
   text-align: center;
-`;
-const PostDescription = styled.p`
-  font: normal normal 400 18px/14px Source Sans Pro;
-  letter-spacing: 1px;
-  color: #040505;
-  margin: 0 1.75rem 3.5rem;
+
   @media (min-width: 600px) {
-    font: normal normal 400 17px/32px Source Sans Pro;
+    font: normal normal normal 25px/32px Source Sans Pro;
   }
 `;
 
-const imageRatio = 3 / 4;
+
+const PostDescription = styled.p`
+  font: normal normal 400 16px/18px Source Sans Pro;
+  letter-spacing: 1px;
+  color: #040505;
+  margin: 0 ;
+  @media (min-width: 600px) {
+    font: normal normal 400 20px/30px Source Sans Pro;
+  }
+  cursor: auto;
+`;
 
 function PostListItem({ post }) {
   const { title, featuredImage, teaser } = post.fields;
   console.log(post.fields.teaser.content[0].content[0].value);
 
   return (
-    <Link href={`/feed/${post.sys.id}`} passHref>
       <PostListItemContainer>
-        <Image
-          src={'https:' + featuredImage.fields.file.url}
-          alt={title}
-          width={300}
-          height={300 / imageRatio}
-          objectFit={'cover'}
-        />
+        <Link href={`/feed/${post.sys.id}`} passHref>
+          <ImageWrapper>
+            <Image
+              src={'https:' + featuredImage.fields.file.url}
+              alt={title}
+              layout={'fill'}
+              objectFit={'cover'}
+              style={{cursor: 'pointer'}}
+            />
+          </ImageWrapper>
+        </Link>
         <PostDetails>
-          <PostTitle>{title}</PostTitle>
+          <Link href={`/feed/${post.sys.id}`} passHref>
+            <PostTitle>{title}</PostTitle>
+          </Link>
           <PostDescription>
             {teaser.content[0].content[0].value}
           </PostDescription>
         </PostDetails>
       </PostListItemContainer>
-    </Link>
   );
 }
 
--- apps/public-ui/pages/feed/[id].tsx
+++ apps/public-ui/pages/feed/[id].tsx
@@ -2,27 +2,36 @@ import styled from 'styled-components';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { createClient } from 'contentful';
 import { fetchPaths } from '../../lib/content/contentfulPosts';
-import { Layout, Page } from '../../components/Layout';
+import { Layout } from '../../components/Layout';
+import Image from 'next/image';
 
 const PostDetailContainer = styled.div`
-  margin: auto;
   max-width: 800px;
+  margin: 2rem auto auto;
+  padding: 1rem;
 `;
 
-const PostImage = styled.img`
+const ImageWrapper = styled.div`
+  position: relative;
+  margin: auto; // Centers the wrapper on the page
   width: 100%;
-  height: 400px;
-  object-fit: cover;
+  padding-bottom: 50%;
 `;
 
 const PostTitle = styled.h1`
-  font-size: 2rem;
+  font: normal normal normal 20px/22px Source Sans Pro;
   text-align: center;
+  @media (min-width: 600px) {
+    font: normal normal normal 25px/32px Source Sans Pro;
+  }
 `;
 
 const PostBody = styled.div`
-  font-size: 1rem;
+  font: normal normal normal 12px/14px Source Sans Pro;
   line-height: 1.5;
+  @media (min-width: 600px) {
+    font: normal normal normal 16px/20px Source Sans Pro;
+  }
 `;
 
 export default function PostDetail({ post }) {
@@ -31,7 +40,15 @@ export default function PostDetail({ post }) {
   return (
     <Layout>
       <PostDetailContainer>
-        <PostImage src={featuredImage.fields.file.url} alt={title} />
+        <ImageWrapper>
+          {/*<PostImage src={featuredImage.fields.file.url} alt={title} />*/}
+          <Image
+            src={'https:' + featuredImage.fields.file.url}
+            alt={title}
+            layout={'fill'}
+            objectFit={'cover'}
+          />
+        </ImageWrapper>
         <PostTitle>{title}</PostTitle>
         <PostBody>{documentToReactComponents(content)}</PostBody>
       </PostDetailContainer>
