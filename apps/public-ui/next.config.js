075bb36f60661a12acf8e79d0d220c8a2a14c3bb
Chad Gallati <chadgallat@gmail.com>
2025-07-26 01:27:40 -0400

    Revert "install sharp"
    This reverts commit 2f7d6ecd9afdf83790f490056c285677eab98649.

--- apps/public-ui/next.config.js
+++ apps/public-ui/next.config.js
@@ -5,6 +5,11 @@ const withNx = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  nx: {
+    // Set this to true if you would like to to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
   reactStrictMode: true,
   images: {
     domains: ['images.ctfassets.net'],
